83315ed07c76acb20020811bc46c7b3a9c6bc6ce
Joseph <joseph28-vasquez@hotmail.com>
2022-03-08 22:27:21 -0500

    Se realizaron cambios para agregar controladores modelos y vista 08/09/2022

---
+++ config/conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+    class Conectar{
+        protected $dbh;
+
+        protected function Conexion(){
+            try{
+                $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=crud_php_mvc","root","");
+                return $conectar;
+            }catch(Exception $e){
+                print "¡Error DB!:". $e->getMessage() ."<br/>";
+                die();
+            }
+        }
+
+        public function set_names(){
+            return $this->dbh->query("SET NAMES 'utf8'");
+        }
+    }
+
+?>
